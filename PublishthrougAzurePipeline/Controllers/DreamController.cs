fcbeb720491080a4c70f04d15411cdd610ca2bd6
Nithiyanantham Navaruban <ananthruban10@gmail.com>
2023-08-13 09:31:41 +0800

    first commit nava 130823

---
+++ PublishthrougAzurePipeline/Controllers/DreamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PublishthrougAzurePipeline.Controllers
+{
+    public class DreamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
